0ec5062547ad676ce89da8548e0ae17415bc3eab
Chinedu Amadi <chinedu0507@gmail.com>
2018-05-02 17:54:51 +0300

    More mobile responsiveness tweaks

--- config/login.php
+++ config/login.php
@@ -3,10 +3,13 @@
 // Login details for connecting to the MySQL database;
 
 define('HOSTNAME','localhost');
-define('DB_USERNAME','edu');
+// define('DB_USERNAME','u923169836_edu');
+// define('DB_DATABASE','u923169836_todo');
 // define('DB_USERNAME','id5128737_chinedu0507');
+// define('DB_PASSWORD','nakane09');
+define('DB_USERNAME','edu');
 define('DB_PASSWORD','batman');
-define('DB_DATABASE','todo'); // Case sensitive?
+define('DB_DATABASE','todo');
 // define('DB_DATABASE','id5128737_todo'); // Case sensitive?
 
 // Password salts
